cb2068b2745a50829e4adbe7542a9c281c3a7b59
Jeff Barczewski <jeff.barczewski@gmail.com>
2010-09-12 00:12:50 -0500

    show extend works without modulize

--- spec/without_modulize_spec.rb
+++ spec/without_modulize_spec.rb
@@ -55,3 +55,30 @@ describe CSubclassedNoMod do
   it { should == "M2#foo/CParentNoMod#foo" } # should still work since method was from parent
 end
 
+
+
+# show extending a single class instance methods with a module works without needing modulize
+
+class CModInstance1
+  def foo
+    "CModInstance1#foo"
+  end
+end
+
+module MModInstance1
+  def foo
+    "MModInstance1#foo/"+super
+  end
+end
+
+describe CModInstance1 do
+  before(:each) do
+    @obj = CModInstance1.new
+    @obj.extend MModInstance1
+  end
+
+  it "will call both M#foo and ClassWithModulize#foo" do
+    @obj.foo.should == "MModInstance1#foo/CModInstance1#foo"
+  end
+end
+
